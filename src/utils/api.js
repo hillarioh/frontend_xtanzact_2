d3679e35bf99bb8530d191a117f4b82d55069bc4
hillarioh <okerioh@gmail.com>
2024-09-12 11:18:29 +0300

    update file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const baseUrl = 'https://wispy-moon-245.fly.dev/api/v1';
-const loginUrl = 'https://wispy-moon-245.fly.dev/login';
+// const baseUrl = 'https://wispy-moon-245.fly.dev/api/v1';
+// const loginUrl = 'https://wispy-moon-245.fly.dev/login';
+const baseUrl = 'http://localhost:3000/api/v1';
+const loginUrl = 'http://localhost:3000/login';
 
 export const get_request = async (url, token) =>
   await axios.get(`${baseUrl}/${url}`, {
